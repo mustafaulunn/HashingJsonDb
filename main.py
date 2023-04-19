fd91bd7d9e25d52642562af2dfa47bed593cad7a
Mustafa <mustafaulun7@gmail.com>
2023-04-19 03:25:58 +0300

    sayhi

--- main.py
+++ main.py
@@ -1,49 +1,62 @@
-class kullanici:
-    def __init__(self,ad,sifre,email,yas):
-        self.ad=ad
-        self.sifre=sifre
-        self.email=email
-        self.yas=yas
+import json
 
 
-class yönetici:
+class Kullanici:
+    def __init__(self, ad, sifre, email, yas):
+        self.ad = ad
+        self.sifre = sifre
+        self.email = email
+        self.yas = yas
+
+class Yönetici:
     def __init__(self):
-        self.kullanicil=[]
-        self.isLoggedIn=False
-        self.mevcut={}
-
+        self.kullanicilar = []
+        self.isLoggedIn = False
 
         self.loadUser()
 
     def loadUser(self):
+        # Implement this method to load user data from some data source
+        # and use it to initialize the self.kullanicilar attribute.
         pass
 
-    def kayıt(self,kullanicil:kullanici):
-        self.kullanicil.append(kullanici)
+    def kayit(self, kullanici: Kullanici):
+        self.kullanicilar.append(kullanici)
         self.loadUser()
         print('Kullanici Oluşturuldu')
+
+    def giris(self, email, sifre):
+        # Implement this method to check if the given email and password
+        # match a user in the self.kullanicilar list.
         pass
 
-    def giriş(self):
-        pass
+    def dosyayayazdirma(self):
+        list=[]
+        for kullanici in self.kullanicilar:
+            list.append(json.dumps(kullanici.__dict__))
 
-    def kayitoluştur(self):
-        pass
+        with open('kullanıcılar.json','w') as file:
+             json.dump(self.kullanicilar,file)
 
 
 #while True:
  #   print('Menü'.center(90))
   #  menusecim = int(input('1-Üyelik Oluştur\n 2-Giriş Yap\n 3-Çıkış Yap 4-Kullanıcı Bilgileri 5-Çıkış Yap'))
    # break
-
+yönet=Yönetici()
 menu = lambda: print('Menü'.center(90)) or int(input('1-Üyelik Oluştur\n 2-Giriş Yap\n 3-Çıkış Yap\n 4-Kullanıcı Bilgileri\n 5-Çıkış Yap\n'))
 
+secim = menu()
 while True:
-    secim = menu()
+
     if secim == 1:
-        username=input('Kullanici Adi')
-        username = input('Kullanici Adi')
-        username=input('Kullanici Adi')
+        ad=input('İsminiz')
+        sifre = input('Sifre Giriniz')
+        email=input(' Mail giriniz')
+        yas = input('Yaş Giriniz1')
+        kullanici = Kullanici(ad=ad, sifre=sifre, email=email, yas=yas)
+        yönet.kayit(kullanici)
+        print(yönet.kullanicilar)
 
     elif secim == 2:
        pass # giriş yapma işlemleri
